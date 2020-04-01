ad13aacf622b47e7955fcfda257ecd5524d73261
Srishti Nath <srishti.nath@gmail.com>
2020-04-01 01:51:49 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -21,9 +21,7 @@ def won?(board)
   value_2 = board[win_combination[1]]
   value_3 = board[win_combination[2]]
 
-    if (value_1 + value_2 + value_3 == 0) || (value_1 + value_2 + value_3).nil?
-      return false
-    elsif (value_1 == "X" && value_2 == "X" && value_3 == "X") || (value_1 == "O" && value_2 == "O" && value_3 == "O")
+    if (value_1 == "X" && value_2 == "X" && value_3 == "X") || (value_1 == "O" && value_2 == "O" && value_3 == "O")
       return win_combination
     else
       return false
